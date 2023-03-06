124688fe877fa3f73e16ac8ee7b4c814fc4cf9fa
Logan <loganed09@gmail.com>
2023-03-06 16:25:33 -0500

    first push

---
+++ game.js
@@ -0,0 +1,70 @@
+let buttonColors = ['red', 'blue', 'green', 'yellow'];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false; 
+
+
+$(document).keydown(() =>{
+    if(!started){
+        $("h1").text(`Level ${level}`)
+        nextSequence();
+        started= true;
+    }
+})
+
+$('.btn').click((e) => {
+    userClickedPattern.push(e.target.id)
+    animatePress(e.target.id);
+    playSound(e.target.id);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+//  Functions
+const nextSequence = () => {
+    userClickedPattern = [];
+    level++;
+    $("h1").text(`Level ${level}`)
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    animatePress(randomChosenColor);
+    playSound(randomChosenColor);
+};
+
+const playSound = (name) => {
+    new Audio(`sounds/${name}.mp3`).play();
+}
+
+const animatePress = (currentColor) => {
+    $('#' + currentColor).addClass('pressed');
+    setTimeout(()=> {
+        $('#' + currentColor).removeClass('pressed');
+    }, 100)
+}
+
+const startOver = () => {
+    level = 0;
+    started = false;
+    gamePattern = [];
+}
+
+const checkAnswer = (currentLevel) => {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        console.log('sucess');
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(() =>{
+                nextSequence();
+            }, 1000)
+        }
+    }else {
+        $("h1").text('Game OVER! Press any key to Restart');
+        $("body").addClass('game-over');
+        setTimeout(()=>{
+            $("body").removeClass('game-over')
+        }, 200);
+        startOver();
+    }
+}
+
+
